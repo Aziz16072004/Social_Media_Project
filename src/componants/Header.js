8f239c8da42d0832fa5c72a044d631c30ea1182a
Aziz16072004 <mouhamedazizchaabani@gmail.com>
2024-01-02 15:07:56 +0100

    updating

--- src/componants/Header.js
+++ src/componants/Header.js
@@ -5,14 +5,14 @@ export default function Header(){
     return(
         <div class="container">
         <header class="row d-sm-flex justify-content-between">
-            <div class="logo col-5 col-md-4 text-center">
+            <div class="logo col-8 col-md-4 text-center">
                 <h2>Social Media</h2>
             </div>
             <div class="searche  d-none col-md-5 d-md-flex col-6 text-center">
                 <img src={searcheImg} alt=""/>
                 <input type="text" placeholder="searche for creator , inspiration and projects"/>
             </div>
-            <div class="header-rigth col-3 text-center">
+            <div class="header-rigth col-4 col-sm-3  text-center">
                 <a href="#create" class="button">Create</a>
             </div>
         </header>
